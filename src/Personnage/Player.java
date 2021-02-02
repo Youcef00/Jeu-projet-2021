2562cfe40f494a132198fe4f88f0962b2b132bee
Haik Phagradiani <haik.phagradiani.etu@univ-lille.fr>
2021-02-02 12:59:35 +0100

    Class Player and WarPlayer

--- src/Personnage/Player.java
+++ src/Personnage/Player.java
@@ -1,5 +1,42 @@
 package Personnage;
 
 public class Player {
+	protected String name;
+	protected int points;
+	
+	
+	public Player(String name) {
+		this.name = name;
+		this.points = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void addPoints(int points) {
+		this.points += points;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Player)) {
+			return false;
+		} 
+		else {
+			Player other = (Player) o;
+			return other.name == this.name && other.points == this.points;
+		}
+	}
+	
 
 }
---
+++ src/Personnage/WarPlayer.java
@@ -0,0 +1,15 @@
+package Personnage;
+
+public class WarPlayer extends Player {
+
+	protected int gold;
+	protected int food;
+	
+	protected int nbWarriors;
+	protected List<Army> army; // a revoir
+	
+	public WarPlayer(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+}
