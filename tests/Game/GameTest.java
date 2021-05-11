839808bdbd4b5d340a26a2550c48adda53df3875
moukeut <youcef.moukeut@univ-lille.fr>
2021-05-11 10:04:34 +0200

    documentation

--- tests/Game/GameTest.java
+++ tests/Game/GameTest.java
@@ -6,14 +6,12 @@ import org.junit.Test;
 
 public class GameTest {
 
-<<<<<<< HEAD
-	
-=======
+
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
->>>>>>> 5c31f15ba4d2e19c265914fa33d3321e96c8b762
+
 	
 
 }
